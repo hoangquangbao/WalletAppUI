f34ee39ee3b91c891f3eef22b2c9f6a28b778fff
Quang Bao <hoangquangbao.developer@gmail.com>
2022-02-27 16:42:13 +0700

    Show Cards

--- WalletAppUI/Model/Card.swift
+++ WalletAppUI/Model/Card.swift
@@ -12,14 +12,15 @@ struct Card: Identifiable {
     
     var id = UUID().uuidString
     var name : String
-    var cardImage : String
+    var cardType : String
     var cardNumber : String
+    var bgCard : String
 }
 
 var cards : [Card] = [
     
-    Card(name: "Quang Bao", cardImage: "Card1", cardNumber: "1210 0121 2992 1229"),
-    Card(name: "Quang Huy", cardImage: "Card2", cardNumber: "0302 2030 0660 0306"),
-    Card(name: "Ha Luu", cardImage: "Card3", cardNumber: "2302 2032 2662 2326")
+    Card(name: "Quang Bao", cardType: "VISA", cardNumber: "1210 0121 2992 1229", bgCard: "bgCard1"),
+    Card(name: "Quang Huy", cardType: "MASTER CARD", cardNumber: "0302 2030 0660 0306", bgCard: "bgCard2"),
+    Card(name: "Ha Luu", cardType: "PAYPAL", cardNumber: "2302 2032 2662 2326", bgCard: "bgCard3")
     
 ]
